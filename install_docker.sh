890bc1fa9d8f817c9033e9a62c2f7beaadb811fe
Galen-Dively <65838051+Galen-Dively@users.noreply.github.com>
2025-01-29 13:10:59 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,12 +1,14 @@
 # Download and Install Docker on Rocky Linux
-sudo dnf check-update
+sudo dnf check-update -y
 echo "Adding repo to package manager"
-sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
 echo "Installing docker"
-sudo dnf install docker-ce docker-ce-cli containerd.io
+sudo dnf install docker-ce docker-ce-cli containerd.io -y
 
 echo "Start docker"
 sudo systemctl start docker
 
 echo "Enabling docker"
 sudo systemctl enable docker
+
+sudo systemctl status docker
