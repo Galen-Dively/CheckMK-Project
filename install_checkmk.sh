2566223f8a0fb5cca6255579b464978e68e159c6
Galen-Dively <65838051+Galen-Dively@users.noreply.github.com>
2025-01-29 13:15:13 -0500

    Create install_checkmk.sh

---
+++ install_checkmk.sh
@@ -0,0 +1,7 @@
+# Script used to install checkmk container and fetch password
+sudo su # switch user to sudo
+
+docker container run -dit -p 8080:5000 -p 8000:8000 --tmpfs /opt/omd/sites/cmk/tmp:uid=1000,gid=1000 -v monitoring:/omd/sites --name checkmk -v /etc/localtime:/etc/localtime:ro --restart always checkmk/check-mk-raw:2.3.0-latest
+
+# get password
+docker container logs checkmk | grep http
